66f0637a5f9bc8e2e33384e2b6d837942b8c0742
Skylar4 <95008499+Skylar4@users.noreply.github.com>
2025-02-12 13:28:12 -0700

    fixed spelling

--- analysis-projec/src/democomponents/FlipWordsDemo.tsx
+++ analysis-projec/src/democomponents/FlipWordsDemo.tsx
@@ -1,12 +1,12 @@
 import { FlipWords } from "../components/ui/flip-words";
 
 export function FlipWordsDemo() {
-  const words = ["better", "asas", "beautiful", "New"];
+  const words = ["better", "beautiful", "New"];
 
   return (
     <div className="h-[40rem] flex justify-center items-center px-4">
       <div className="text-4xl mx-auto font-normal text-neutral-600 dark:text-neutral-400">
-        Welcome
+        Welcome, feel
         <FlipWords words={words} /> <br />
         Save your Life
       </div>
