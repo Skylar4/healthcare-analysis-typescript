7afb2004f13a5fe3499d7a9e13658de542e55f5f
RobinSuthar <152766032+RobinSuthar@users.noreply.github.com>
2025-02-12 13:32:57 -0700

    Navigation to home, Docs, AboutPage done

---
+++ analysis-projec/src/democomponents/NagiationDemo.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { Link } from "react-router-dom";
+
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from "../components/ui/navigation-menu";
+
+export function NavigationMenuDemo() {
+  return (
+    <NavigationMenu>
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <Link to="/">
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              Home
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <Link to="/docs">
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              Documentation
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <Link to="/aboutus">
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              Aboutus
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
+}
