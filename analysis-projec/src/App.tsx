a8ff753526ea9ebc89a9ec24ac7793205a27d635
RobinSuthar <152766032+RobinSuthar@users.noreply.github.com>
2025-02-12 13:23:04 -0700

    Added theme provider for light and dark mode

--- analysis-projec/src/App.tsx
+++ analysis-projec/src/App.tsx
@@ -2,19 +2,23 @@ import MainPage from "./pages/MainPage";
 import AboutPage from "./pages/Aboutpage";
 import InfoPage from "./pages/InfoPage";
 import Result from "./pages/Result";
-
+import { ThemeProvider } from "./components/theme-provider";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ModeToggle } from "./components/mode-toogle";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MainPage />}></Route>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <ModeToggle></ModeToggle>
+        <Routes>
+          <Route path="/" element={<MainPage />}></Route>
 
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/infopage" element={<InfoPage />} />
-        <Route path="/result" element={<Result />} />
-      </Routes>
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/infopage" element={<InfoPage />} />
+          <Route path="/result" element={<Result />} />
+        </Routes>
+      </ThemeProvider>
     </BrowserRouter>
   );
 }
