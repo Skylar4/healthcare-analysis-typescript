40f77cc48a14032ae8d430da9cd9e10f8c9a51a5
RobinSuthar <152766032+RobinSuthar@users.noreply.github.com>
2025-03-19 16:43:43 -0600

    Resolving issue

--- analysis-projec/src/pages/MainPage.tsx
+++ analysis-projec/src/pages/MainPage.tsx
@@ -1,11 +1,9 @@
 import { FlipWordsDemo } from "../democomponents/FlipWordsDemo";
-import { Button } from "../components/ui/button";
 
 const MainPage = () => {
   return (
     <div>
       <FlipWordsDemo />
-      <Button variant="outline">Button</Button>
     </div>
   );
 };
